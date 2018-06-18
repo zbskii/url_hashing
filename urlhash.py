72a69a516d453f0ed59ef5b2c3cbc0180ec55fc5
Brett Carter <bcarter@appnexus.com>
2018-06-18 10:00:34 -0400

    Refactor

--- urlhash.py
+++ urlhash.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import hashlib
 import urlparse
 import os.path
@@ -6,46 +5,51 @@ import os.path
 TMPDIR="/tmp/urlhasher"
 
 """
-Generate nice zero padded hex strings with no leading 0x
+Hash urls into pre-determined partitions
 """
-def digits():
-    return [hex(x)[2:].zfill(4) for x in range(65536)]
 
-"""
-Check to see if our hex partitions exist
-"""
-def createdirs():
-    if(not os.path.exists(TMPDIR)):
-        os.mkdir(TMPDIR)
-    os.chdir(TMPDIR)
-    for h in digits():
-        if not(os.path.exists(h)):
-            os.mkdir(TMPDIR + '/' + h)
+class UrlHasher():
 
-"""
-Entrypoint
-"""
-def main():
-    createdirs()
-    while True:
-        u = raw_input("Enter a url: ")
-        result = urlparse.urlparse(u)
-        # Somebody input junk
-        if (not result[1]):
-            print "Bad url!"
-            return(0)
-        # hash the url
-        h = hashlib.sha256(u).hexdigest()
-        # find the diretory
-        uf = TMPDIR + '/' + h[:4]
-        # See if the url exists, if so print it out
-        if os.path.exists(uf):
-            print open(uf).readlines()
-        else:
-            print "File not found in %s saving..." % uf
-            fp = open(uf, 'w')
-            fp.write(u)
-            print "Done!"
+    """
+    Generate nice zero padded hex strings with no leading 0x
+    """
+    def digits(self):
+        return [hex(x)[2:].zfill(4) for x in range(65536)]
 
-if __name__ == "__main__":
-    exit(main())
+    """
+    Check to see if our hex partitions exist
+    """
+    def createdirs(self):
+        if(not os.path.exists(TMPDIR)):
+            os.mkdir(TMPDIR)
+            os.chdir(TMPDIR)
+        for h in self.digits():
+            hashdir = TMPDIR + '/' + h
+            if not(os.path.exists(hashdir)):
+                os.mkdir(hashdir)
+
+    """
+    Entrypoint
+    """
+    def main(self):
+        self.createdirs()
+        while True:
+            u = raw_input("Enter a url: ")
+            result = urlparse.urlparse(u)
+            # Somebody input junk
+            if (not result[1]):
+                print "Bad url!"
+                return(0)
+            # hash the url
+            h = hashlib.sha256(u).hexdigest()
+            print "Got hash %s" % h
+            # find the diretory
+            partf = TMPDIR + '/' + h[:4] + '/' + h
+            # See if the url exists, if so print it out
+            if os.path.exists(partf):
+                print open(partf).readlines()
+            else:
+                print "File not found in %s saving..." % partf
+                fp = open(partf, 'w')
+                fp.write(u)
+                print "Done!"
